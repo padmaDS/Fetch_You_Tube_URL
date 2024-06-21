30151d7b99e68e0f1ff175796bee28c27e65719a
Padma Vathi <105155330+padmaDS@users.noreply.github.com>
2024-06-21 13:38:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import scrapetube
+import pandas as pd
+
+# Prompt for channel_id
+channel_id = input("Enter the Channel_id: ")
+
+# Initialize list for URLs
+url_list = []
+
+# Base URL for YouTube videos
+base_url = "https://www.youtube.com/watch?v="
+
+# Fetch videos from the channel
+videos = scrapetube.get_channel(channel_id)
+
+# Loop through videos and collect URLs
+for video in videos:
+    video_url = base_url + video['videoId']
+    print(video_url)
+    url_list.append(video_url)
+
+# Create a DataFrame with collected URLs
+dataframe = pd.DataFrame(url_list, columns=["URL"])
+
+# Save DataFrame to an Excel file
+dataframe.to_excel("tv9urls.xlsx", index=False)
+
+print("Task is completed")
